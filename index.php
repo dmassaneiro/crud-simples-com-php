0334f89f556a0457cce262713e98e77bae937d1d
Diego Massaneiro <dmassaneiro95@gmail.com>
2020-06-11 17:13:01 -0300

    Criação da tela para realizar as operações

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once "./app/conexao/Conexao.php";
 include_once "./app/dao/UsuarioDAO.php";
 include_once "./app/model/Usuario.php";
@@ -7,32 +7,38 @@ include_once "./app/model/Usuario.php";
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>CRUD Simples PHP 2</title>
     <style>
-        body{
-            background-color: #bbb;
+        .menu,thead{
+            background-color: #bbb !important;
         }
-        .row{
+
+        .row {
             padding: 10px;
         }
     </style>
 </head>
+
 <body>
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">
-        CRUD PHP POO
-  </a>
-</nav>
-<div class="container">
+    <nav class="navbar navbar-light bg-light menu">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                CRUD PHP POO
+            </a>
+        </div>
+    </nav>
+    <div class="container">
+        <form action="app/controller/UsuarioController.php" method="POST">
         <div class="row">
             <div class="col-md-3">
                 <label>Nome</label>
                 <input type="text" name="nome" value="" class="form-control" require />
-            </div> 
+            </div>
             <div class="col-md-5">
                 <label>Sobrenome</label>
                 <input type="text" name="sobrenome" value="" class="form-control" require />
@@ -48,12 +54,31 @@ include_once "./app/model/Usuario.php";
                     <option value="F">Feminino</option>
                 </select>
             </div>
-            <hr>
+            <div class="col-md-2">
+                <br>
+                <button class="btn btn-primary" type="submit" name="cadastrar" >Cadastrar</button>
+            </div>
+        </div>
+        </form>
+        <hr>
+        <div class="table-responsive">
+            <table class="table table-sm table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Sobrenome</th>
+                        <th>Idade</th>
+                        <th>Sexo</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+            </table>
         </div>
 
-</div>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
+
 </html>
