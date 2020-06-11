40764195213e8bcbac030bf175154cc0c47070e8
Diego Massaneiro <dmassaneiro95@gmail.com>
2020-06-11 16:35:57 -0300

    Criado a Classe Usuario

---
+++ app/model/Usuario.php
@@ -0,0 +1,54 @@
+<?php
+
+class Usuario{
+    
+    private $id;
+    private $nome;
+    private $sobrenome;
+    private $idade;
+    private $sexo;
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getSobrenome() {
+        return $this->sobrenome;
+    }
+
+    function getIdade() {
+        return $this->idade;
+    }
+
+    function getSexo() {
+        return $this->sexo;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setSobrenome($sobrenome) {
+        $this->sobrenome = $sobrenome;
+    }
+
+    function setIdade($idade) {
+        $this->idade = $idade;
+    }
+
+    function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+
+    
+}
+
